e4321b6f591bde3a68d0d02ef9ad42b9f94ec8b1
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-07-15 01:49:53 +0530

    list completed

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -11,12 +11,27 @@ const TodoList= () => {
 
   let filterTodos = todos;
 
+  const toggleTodoAsCompleted = (id: string) => {
+    todoStore.toggleTodoAsCompleted(id);
+  };
+
+  const deleteTodo = (id: string) => {
+    todoStore.deleteTodo(id);
+  };
+
   return (
-    <div>
+    <ul>
       {filterTodos.map((todo) => (
-        <li key={todo.id}>{todo.task}</li>
+        <li key={todo.id} className='bg-[#596697cb] p-2 w-max text-center rounded-lg outline-none flex items-center justify-center border-t-[#fea49f] border-b-[#fea49f] border-[1px] border-l-[0px] border-r-[0px]'>
+          <input type="checkbox" name="" id={`todo-${todo.id}`} className='mr-2' checked={todo.completed} onChange={() => toggleTodoAsCompleted(todo.id)}  />
+          <label htmlFor={`todo-${todo.id}`} className='ml-2 mr-2 font-bold text-[#d0bdf4] '>{todo.task}</label>
+          { 
+            todo.completed && <button type="button" className='ml-3 mt-1 border-b-[1px] border-[white] text-[15px] text-center text-[#fea49f]' onClick={() => deleteTodo(todo.id)}> Delete </button>
+          }
+        </li>
+
       ))}
-    </div>
+    </ul>
   );
 };
 
